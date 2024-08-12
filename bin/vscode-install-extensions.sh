606d9777f2891424d7010334f0892662dfea857e
Chris Grove <chris.grove@vansysinc.com>
2024-08-12 10:39:33 -0400

    Simplified, removed VSCode (now syncing with profiles)

--- bin/vscode-install-extensions.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "Installing VSCode extensions from list in dotfiles"
-
-# Read each line in ~/.dotfiles/vscode_extensions and install the named extension
-while read line; do
-  code --install-extension "$line"
-done < ~/.dotfiles/vscode_extensions
--- bin/vscode-save-extensions.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-echo "Saving VSCode extension list to dotfiles"
-code --list-extensions > "$HOME"/.dotfiles/vscode_extensions
