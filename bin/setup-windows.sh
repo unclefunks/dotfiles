e0e5f8f41a5959e4a2e361740710a046b9a3cc0e
Chris Grove <cgrove99@gmail.com>
2025-03-25 09:54:31 -0400

    Update 2025-03-25T13:54:31Z

--- bin/setup-windows.sh
+++ bin/setup-windows.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/env bash
+DOTFILES=$USERPROFILE/.dotfiles
 
-# Prompt and font
-choco install firacode --yes
+# Starship prompt
 winget install starship
+
+# FiraCode nerd fonts
+find "$DOTFILES/tag-windows/FiraCode" -name "*.ttf" -exec cp {} "/c/Windows/Fonts" \;
 
 # No version of rcm on Windows for dotfile management; also symbolic links are a pain.
 # https://stackoverflow.com/questions/18641864/git-bash-shell-fails-to-create-symbolic-links/40914277#40914277.
 # cmd //c "mklink /J %USERPROFILE%\.zshrc %USERPROFILE%\.dotfiles\zshrc"
 # cmd //c "mklink /J %USERPROFILE%\.bashrc %USERPROFILE%\.dotfiles\bashrc"
 # cmd //c "mklink /J %USERPROFILE%\.config\starship.toml %USERPROFILE%\.dotfiles\config\starship.toml"
-DOTFILES=$USERPROFILE/.dotfiles
 mkdir -p "$USERPROFILE/.config"
 cp "$DOTFILES/zshrc" "$USERPROFILE/.zshrc"
 cp "$DOTFILES/bashrc" "$USERPROFILE/.bashrc"
